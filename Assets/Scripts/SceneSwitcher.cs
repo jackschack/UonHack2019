757f9a9455682a97ef1bd61222fbc8c55a711186
Ejay <Ethan.moriarty@gmail.com>
2019-05-11 14:37:16 +1000

    Main Menu Finished

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void GotoPlayScene()
+    {
+        SceneManager.LoadScene("Play");
+    }
+
+    public void GotoARScene()
+    {
+        SceneManager.LoadScene("AR");
+    }
+
+    public void GotoSoSScene()
+    {
+        SceneManager.LoadScene("SoS");
+    }
+}
